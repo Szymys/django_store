9ca7358d6fbf74e6a8aaee2ed25c0158f76e5575
Szymon <103196453+Szymys@users.noreply.github.com>
2024-06-19 17:55:13 +0200

    Add files via upload

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'store.views.categories',  #dodanie views z folderu store
             ],
         },
     },
@@ -122,7 +123,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
+MEDIA_URL = '/media/'
 
+MEDIA_ROOT = BASE_DIR / 'static/media'
 
 
 
