668f6e210e0969e01b427ea1ee2abdc986127b6d
Szymon <103196453+Szymys@users.noreply.github.com>
2024-08-01 20:33:34 +0200

    csrf token

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -12,7 +12,10 @@ from  django.http import JsonResponse
 
 def cart_summary(request):
 
-    return render(request, 'cart/cart-summary.html')
+    cart = Cart(request)
+
+
+    return render(request, 'cart/cart-summary.html', {'cart':cart} )
 
 
 def cart_add(request):
@@ -31,7 +34,13 @@ def cart_add(request):
 
         cart.add(product=product, product_qty=product_quantity)
 
-        response = JsonResponse({'The product is called: ': product.title, ' and the product quantity is:': product_quantity})
+
+
+        cart_quantity = cart.__len__()
+
+
+
+        response = JsonResponse({'qty': cart_quantity})
 
         return response
 
